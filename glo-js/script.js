e7ff9b090b0ce3b6345a50c939b3049f096c4b3a
Gla3nata <0207natalya@gmail.com>
2025-10-17 18:06:58 +0600

    homework lesson2 task1

--- glo-js/script.js
+++ glo-js/script.js
@@ -1,9 +1,19 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let title = "study-glo";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 484930;
+let rollback = 13;
+let fullPrice = 100000;
+let adaptive = true;
 
-alert("Привет мир");
-console.log("Это тестовое сообщение в консоли");
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+
+console.log(screens.length);
+console.log("Стоимость верстки экранов " + screenPrice +" рублей. " + "Стоимость разработки сайта " +fullPrice +" рублей" );
+
+console.log(screens.toLowerCase());
+screens = screens.split(/\s*,\s*/);
+console.log(screens);
+
+console.log("Процент отката посреднику за работу " + fullPrice * (rollback/100));
