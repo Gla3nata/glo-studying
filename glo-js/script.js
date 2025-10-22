b937672b478fa97a70bb528fc058aee32cc5bfbc
Gla3nata <0207natalya@gmail.com>
2025-10-22 09:20:38 +0600

    task 1-2

--- glo-js/script.js
+++ glo-js/script.js
@@ -1,38 +1,52 @@
 'use strict';
 
-const title = prompt("Как называется ваш проект");
-const screens = prompt("Какие типы экранов нужно разработать Например: 'Простые, Сложные, Интерактивные'");
-const screenPrice = prompt("Сколько будет стоить данная работа?");
-const adaptive = confirm("Нужен ли адаптив на сайте?");
-
-const expenses1 = prompt('Какой дополнительный тип услуги нужен?', 'анимация');
-const servicePrice1 = prompt("Сколько это будет стоить?", '3000');
-const expenses2 = prompt('Какой дополнительный тип услуги нужен?', 'форма обратной связи');
-const servicePrice2 = prompt("Сколько это будет стоить?", '5500');
-
-const fullPrice = (Number(screenPrice)) + (Number(servicePrice1)) + (Number(servicePrice2));
-
+let title = prompt("Как называется ваш проект");
+let screens = prompt("Какие типы экранов нужно разработать Например: 'Простые, Сложные, Интерактивные'");
+let screenPrice = prompt("Сколько будет стоить данная работа?");
+let adaptive = confirm("Нужен ли адаптив на сайте?");
+let service1 = prompt('Какой дополнительный тип услуги нужен?', 'анимация');
+let servicePrice1 = prompt("Сколько это будет стоить?", '3000');
+let service2 = prompt('Какой дополнительный тип услуги нужен?', 'форма обратной связи');
+let servicePrice2 = prompt("Сколько это будет стоить?", '5500');
 let rollback = 10;
-rollback = fullPrice * (rollback/100);
+let fullPrice = screenPrice + servicePrice1 + servicePrice2;
+let servicePercentPrice = fullPrice - (fullPrice * (rollback/100));
 
-const servicePercentPrice = fullPrice - rollback;
-console.log("Итог: " + Math.ceil(servicePercentPrice));
-
-if (fullPrice > 30000) {
-    console.log('Даем скидку в 10%');
-} else if ( fullPrice >= 15000 && fullPrice <= 30000) {
-    console.log('Даем скидку в 5%');
-} else if (fullPrice < 15000 && fullPrice > 0) {
-    console.log('Скидка не предусмотрена');
-}  else {
-    console.log('Что то пошло не так');
+const showTypeOf = function(variable) {
+    console.log(variable, typeof variable);
 }
 
 
-// 10) Написать конструкцию условий (расчеты приведены в рублях) (вывести в консоль)
-//   - Если fullPrice больше 30000, то “Даем скидку в 10%” 
-//   - Если fullPrice больше 15000 и меньше 30000, то сообщение “Даем скидку в 5%” 
-//   - Если fullPrice меньше 15000 и больше 0 то в консоль вывести сообщение “Скидка не предусмотрена” 
-//   - Если отрицательное значение то вывести “Что то пошло не так” 
-//   - Учесть варианты 0, 15000 и 30000(к какому уровню не важно)
+const getrollbackMessage = function (price) {
+if (price >= 30000) {
+    return 'Даем скидку в 10%'
+} else if ( price >= 15000 && price < 30000) {
+    return 'Даем скидку в 5%'
+} else if (price >= 0 && price < 15000) {
+    return'Скидка не предусмотрена'
+}  else {
+    return 'Что то пошло не так'
+}
+}
+const getAllServicePrices = function () {
+   let allServicePrices = servicePrice1 + servicePrice2
+   return allServicePrices;
+}
+function getFullPrice () {
+   let fullPrice = screenPrice + getAllServicePrices()
+   return fullPrice;
+}
 
+showTypeOf(title);
+showTypeOf(screenPrice);
+showTypeOf(adaptive);
+
+console.log(getrollbackMessage(fullPrice));
+console.log(typeof title);
+console.log(typeof screenPrice);
+console.log(typeof adaptive);
+
+console.log(screens.length);
+console.log(servicePercentPrice);
+
+console.log("Итог: " + Math.ceil(servicePercentPrice));
