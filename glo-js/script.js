57f67361eb77ca75dc202cf010f6196c60396331
Gla3nata <0207natalya@gmail.com>
2025-10-28 00:30:02 +0600

    change lesson8

--- glo-js/script.js
+++ glo-js/script.js
@@ -2,72 +2,70 @@
 
 const appData = {
     title: '',
-    screens: '',
+    screens: [],
     screenPrice: 0,
     adaptive: true,
     rollback: 10,
     allServicePrices: 0,
     fullPrice: 0,
     servicePercentPrice: 0,
-    service1: '',
-    service2: '',
+    services: {},
     start: function () {
-        this.asking();
-        this.allServicePrices = this.getAllServicePrices();
-        this.fullPrice = this.getFullPrice();
-        this.servicePercentPrice = this.getServicePercentPrices();
-        this.title = this.getTitle();
-        this.logger();
+        appData.asking();
+        appData.getAllServicePrices();
+        appData.getFullPrice();
+        appData.getServicePercentPrices();
+        appData.getTitle();
+        appData.logger();
     },
     asking: function () {
         appData.title = prompt('Как называется ваш проект', 'Калькулятор');
-        appData.screens = prompt('Какие типы экранов нужно разработать', 'Простые, Сложные, Интерактивные');
+        // appData.screens = prompt('Какие типы экранов нужно разработать');
 
-        do {
-            appData.screenPrice = prompt('Сколько будет стоить данная работа?');
-        } while (!appData.isNumber(appData.screenPrice))
-
-        appData.adaptive = confirm('Нужен ли адаптив на сайте?');
-    },
-    logger: function () {
-        for (let key in this) {
-            if (typeof this[key] !== 'function') {
-            console.log(key + ': ' + this[key]);
-        }
-        }
-    },
-
-    isNumber: function (num) {
-        return !isNaN(parseFloat(num)) && isFinite(num)
-    },
-    getAllServicePrices: function () {
-        let sum = 0
+        // do {
+        //     appData.screenPrice = prompt('Сколько будет стоить данная работа?');
+        // } while (!appData.isNumber(appData.screenPrice))
 
         for (let i = 0; i < 2; i++) {
-            let price;
-
-            if (i === 0) {
-                appData.service1 = prompt('Какой дополнительный тип услуги нужен?');
-            } else if (i === 1) {
-                appData.service2 = prompt('Какой дополнительный тип услуги нужен?');
-            }
-
+            let name = prompt("Какие типы экранов нужно разработать")
+            let price = 0;
             do {
                 price = prompt('Сколько будет стоить данная работа?');
             } while (!appData.isNumber(price))
-            sum += +price;
+
+            appData.screens.push({ id: i, name: name, price: price })
         }
-        return sum
+        for (let i = 0; i < 2; i++) {
+            let name = prompt("Какой дополнительный тип услуги нужен?")
+            let price = 0;
+
+            do {
+                price = prompt('Сколько будет стоить данная работа?(дополнительная)');
+            } while (!appData.isNumber(price))
+
+            appData.services[name] = +price
+        }
+
+        appData.adaptive = confirm('Нужен ли адаптив на сайте?');
     },
+    getAllServicePrices: function () {
+        for (let key in appData.services) {
+            appData.allServicePrices += appData.services[key]
+        }
+    },
+    isNumber: function (num) {
+        return !isNaN(parseFloat(num)) && isFinite(num)
+    },
+
     getFullPrice: function () {
-        return +appData.screenPrice + appData.allServicePrices
+        appData.fullPrice = +appData.screenPrice + appData.allServicePrices
     },
     getServicePercentPrices: function () {
-        return appData.fullPrice - (appData.fullPrice * (appData.rollback / 100));
+        appData.servicePercentPrice = appData.fullPrice - (appData.fullPrice * (appData.rollback / 100));
     },
     getTitle: function () {
         let processedTitle = appData.title.trim().toLowerCase();
-        return processedTitle.charAt(0).toUpperCase() + processedTitle.slice(1);
+        appData.title = processedTitle.charAt(0).toUpperCase() + processedTitle.slice(1);
     },
     getrollbackMessage: function (price) {
         if (price >= 30000) {
@@ -79,6 +77,14 @@ const appData = {
         } else {
             return 'Что то пошло не так'
         }
+    },
+        logger: function () {
+        for (let key in this) {
+            if (typeof this[key] !== 'function') {
+                console.log(key + ': ' + this[key]);
+            }
+        }
+        console.log(this.screenPrice + 'screenPrice')
     }
 }
 
